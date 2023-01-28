937c324eb8245830fe0a0eae21337af5a44d66af
sudonick <singhnikit16@gmail.com>
2023-01-28 20:50:35 +0530

    init server

---
+++ server/server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/sudo-nick16/smark/server/graph"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	schema := graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}})
+
+	srv := handler.NewDefaultServer(schema)
+
+	http.Handle("/nikit", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello Friend"));
+	}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
