31a0f870a9d88b9d714100cb6782a53028fc422d
sudonick <singhnikit16@gmail.com>
2023-06-01 03:56:10 +0530

    fix(web): fix build block bc of watch options

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,18 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: '../extension/options',
+        outDir: "./dist",
         emptyOutDir: true,
-        watch: {
-            buildDelay: 0,
-            clearScreen: true,
-            skipWrite: false,
-            include: ['src/*.ts', 'src/**/*.ts', 'src/**/*.tsx'], 
-        }
     },
-    base: ""
-})
+    base: "",
+});
